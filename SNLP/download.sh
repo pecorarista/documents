53c3ae25e72ad4d68b4bf3e1bb0489f02c3908f9
Miyazawa Akira <pecorarista@gmail.com>
2016-09-18 09:01:26 +0900

    Update the slide for 最先端NLP勉強会

---
+++ SNLP/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget "http://aclweb.org/anthology/P/P16/P16-1021.pdf"
+wget "https://farm2.staticflickr.com/1063/1070785383_23ceaf0080_o_d.jpg" # rollercoaster
+wget "https://farm3.staticflickr.com/2699/4185145998_0aed3f43b2_o_d.jpg" # boat
+wget "https://farm4.staticflickr.com/3377/3438933381_a7709fc2e5_o_d.jpg"
